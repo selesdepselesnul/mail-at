2e46b710717e79dc45ba0b550e585d52a5a8c9b4
Moch Deden <moch.deden.r@gmail.com>
2018-03-20 11:06:20 +0700

    Add run-at function

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.draines/postal "2.0.2"]]
+                 [com.draines/postal "2.0.2"]
+                 [zcaudate/hara.io.scheduler "2.8.2"]]
   :main ^:skip-aot mail-at.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/mail_at/core.clj
+++ src/mail_at/core.clj
@@ -1,6 +1,8 @@
 (ns mail-at.core
   (:gen-class)
-  (:require [postal.core :as postal]))
+  (:require [postal.core :as postal]
+            [hara.io.scheduler :as scheduler]
+            [clojure.string :as str]))
 
 (defn send-email [prop pass]
   (let [{:keys [host user email subject body receivers]} prop]
@@ -14,6 +16,17 @@
         :to receiver
         :subject subject
         :body body}))))
+
+(defn run-at [time handler]
+  (let [time-splitted (str/split time #":")]
+    (scheduler/start!
+     (scheduler/scheduler
+        {:print-task
+         {:handler handler
+          :schedule
+          (format "01 %s %s * * * *"
+                  (nth time-splitted 1)
+                  (nth time-splitted 0))}}))))
 
 (defn -main
   [& args]
